6a94fcc8dc6093f7594d40df26e8a7a6ce825a30
catchPH <40880680+catchPH@users.noreply.github.com>
2018-07-17 22:33:30 +0800

    update

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -3,12 +3,7 @@
 $recepient = 'iamtherealblackfish@gmail.com';
 $sender = "From: " . $_POST['fromName'] . "<" . $_POST['fromEmail'] . ">\r\n";
 $subject = $_POST['subject'];
-$message = $_POST["message"];
-
-
-/*ini_set("SMTP", $_POST["fromEmail"]);
-ini_set("smtp_port","25");
-ini_set("send_email", $_POST["to"]);*/
+$message = $_POST['message'];
 
 if(mail($recepient, $subject, $message, $sender))
 {
