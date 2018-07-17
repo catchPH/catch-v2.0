72fdf135d47876472a89fd2db1f47d757dc111e6
catchPH <40880680+catchPH@users.noreply.github.com>
2018-07-18 00:07:51 +0800

    try update

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,12 +1,12 @@
 <?php 
 
 $recepient = 'iamtherealblackfish@gmail.com';
-$subject = 'new message has arrived!';
+/*$subject = 'new message has arrived!';
 $message = "Name: " .$_POST['fromName'] ."\n"
  ."Email Address: " .$_POST['fromEmail'] ."\n"
- ."Message: " .$_POST['message'];
+ ."Message: " .$_POST['message'];*/
 
-if(mail($recepient, $subject, $message))
+if(mail($recepient, 'new message has arrived!', 'hello world'))
 {
 	print "<p class='success'>Mail Sent.</p>";
 }
