4d34db207f779f5638d3f4b5f4b374004f54a61b
catchPH <40880680+catchPH@users.noreply.github.com>
2018-07-20 23:46:38 +0800

    update website
    updated navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,9 +27,9 @@ var btns = navigation.getElementsByClassName("links");
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
 	btns[i].addEventListener("click", function() {
-	var current = document.getElementsByClassName("active");
-	current[0].className = current[0].className.replace(" active", "");
-		this.className += " active";
+	var current = document.getElementsByClassName("actived");
+	current[0].className = current[0].className.replace(" actived", "");
+		this.className += " actived";
 	});
 }
 /* ---------- Start Codes for Add/Remove .active class in navigation ---------- */
@@ -204,3 +204,84 @@ display("web")
 	});
 };
 /* ---------- End Codes for Filter Services ---------- */
+
+
+/* ---------- Start Codes for Validate/Send Email with button loading ---------- */
+
+function sendEmail()
+{ 
+	var from_name = $('#name').val();
+	var from_email = $('#email').val();
+	var message = $('#message').val();
+	var data_string = 'name='+from_name+'&email='+from_email+'&message='+message;
+	var valid;
+	valid = validateEmail();
+
+	if(valid)
+	{	
+		$('.contact-button').addClass('loading');
+		$.ajax({
+			method: 'POST',
+			url: 'assets/php/contact.php',
+			data: data_string,
+			dataType: 'json',
+			success: function(data)
+			{
+				console.log(data);
+				if(data.status == 'sent')
+				{
+					setTimeout(function(){
+						$('.contact-button').addClass('hide');
+						$('.done').addClass('finish');	
+						$('.contact-input').val('');
+					}, 3000)
+				}
+				else
+				{
+					$('.contact-button').addClass('hide');
+					$('.failed').addClass('finish');
+					$('.contact-input').val('');					
+				}
+				setTimeout(function(){
+					$('.contact-button').removeClass('loading');
+				    $('.contact-button').removeClass('hide');
+				    $('.done').removeClass('finish');
+				    $('.failed').removeClass('finish');
+				}, 5000);
+			}
+		})
+	}
+}
+
+function validateEmail()
+{
+	var valid = true;
+	$('.info').html('');
+	if(!$('#name').val())
+	{
+		$('#lblName').html("<i class='icon-close' aria-hidden='true'></i> name is required");
+		$('#lblName').css('color', 'red');
+		valid = false;
+	}
+	if(!$('#email').val())
+	{
+		$('#lblEmail').html("<i class='icon-close' aria-hidden='true'></i> email is required");
+		$('#lblEmail').css('color', 'red');
+		valid = false;
+	}
+	if(!$('#email').val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/))
+	{
+		$('#lblEmail').html("<i class='icon-close' aria-hidden='true'></i> invalid email");
+		$('#lblEmail').css('color', 'red');
+		valid = false;
+	}
+	if(!$('#message').val())
+	{
+		$('#lblMessage').html("<i class='icon-close' aria-hidden='true'></i> message is required");
+		$('#lblMessage').css('color', 'red');
+		valid = false;
+	}
+	return valid;
+}
+
+/* ---------- End Codes for Validate/Send Email with button loading ---------- */
